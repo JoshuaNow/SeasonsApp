33c34dff3b933c9fa7aaf9b717e092009603f91e
Joshua Lopez <jrenelopez92@gmail.com>
2021-08-30 18:58:55 -0500

    useState in a class component

--- src/index.js
+++ src/index.js
@@ -16,13 +16,38 @@ import ReactDOM from "react-dom";
 //   return <div>hello there</div>;
 // };
 
+// Rules of state
+//1. state is a JS object that contains data relevant to a component
+//2/ updating 'state' on a component causes the components to almost instantly rerender
+//3. state must be initialized when a component is created
+// 4. STATE CAN ONLY BE UPDATED USING THE FUNCTION 'setState
+
+//TODO
+// initialize state
+// render state
+// update state
 class App extends React.Component {
-  render() {
+  // this is not required by react it is specific to javaScript not react
+
+  constructor(props) {
+    super(props); // super a reference to the parents constructor functions
+    // this is the only time we do a direct assingment to this.state
+    this.state = { lat: null }; // we don't know the latitude yet so we need a place holder number with null
+
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
+      (position) => {
+        // we called setState
+        this.setState({ lat: position.coords.latitude });
+      },
       (err) => console.log(err),
+      // we do not do this.
+      //this.state.lat = position.coords.latitude;
     );
-    return <div>Latitude: </div>;
+  }
+
+  // React says we haver to define render!!
+  render() {
+    return <div>{this.state.lat} </div>;
   }
 }
 
